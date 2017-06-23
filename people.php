73fa522b4d6827dddb7460f88aa101b9473045c1
Jan Hajic <hajic@ufal.mff.cuni.cz>
2017-06-23 19:52:33 +0200

    Update people.php

--- people.php
+++ people.php
@@ -10,6 +10,63 @@ viewPageHeader($title);
 viewArticleHeader($title);
 ?>
 
+  <h2>Program committee chairs</h2>
+<ul>
+	<li>Jan Hajic (Charles University in Prague, Czech Republic)</li>
+	<li>Sandra Kübler (Indiana University, USA)</li>
+	<li>Stephan Oepen (University of Oslo, Norway) </li>
+	<li>Markus Dickinson (Indiana University, USA)</li>
+</ul>
+
+<h2>Program committee </h2>
+
+<ul>
+<li>Patricia Amaral (Indiana University, Bloomington, USA)
+	</li><li>Emily Bender (University of Washington, Seattle, USA)
+</li><li>Eckhard Bick (University of Southern Denmark, Denmark)
+</li><li>Ann Bies (Linguistic Data Consortium, Univ. of Pennsylvania, USA)
+</li><li>Gosse Bouma (University of Groningen, The Netherlands)
+</li><li>Miriam Butt (University of Konstanz, Germany)
+</li><li>Jinho Choi (Emory University, Atlanta, Georgia, USA)
+</li><li>Silvie Cinkova (Charles University, Prague, Czech Republic)
+</li><li>Koenraad De Smedt (Bergen University, Norway)
+</li><li>Tomaz Erjavec (Institut Jozef Stefan, Ljubljana, Slovenia)
+</li><li>Filip Ginter (Turku University, Finland)
+</li><li>Memduh Gokirmak (Instanbul University, Turkey)
+</li><li>Eva Hajicova (Charles University, Prague, Czech Republic)
+</li><li>Dag Haug (University of Oslo, Norway)
+</li><li>Barbora Hladka (Charles University, Prague, Czech Republic)
+</li><li>Lori Levin (Carnegie Mellon University, Pittsburgh, Pennsylvania, USA)
+</li><li>Teresa Lynn (Dublin City University, Ireland)
+</li><li>Marie-Catherine de Marneffe (Ohio State University, Columbus, USA)
+</li><li>Adam Meyers (New York University, New York, USA)
+</li><li>Emad Mohamed (Indiana University, Bloomington, USA)
+</li><li>Jiri Mirovsky (Charles University, Prague, Czech Republic)
+</li><li>Kaili Muurisep (University of Tartu, Estonia)
+</li><li>Joakim Nivre (Uppsala University, Sweden)
+</li><li>Petya Osenova (Bulgarian Academy of Sciences, Sofia, Bulgaria)
+</li><li>Lilja Ovrelid (University of Oslo, Norway)
+</li><li>Agnieszka Patejuk (Polish Academy of Sciences, Warsaw, Poland)
+</li><li>Tatjana Scheffler (University of Potsdam, Germany)
+</li><li>Olga Scrivner (University of Indiana, Bloomington, USA)
+</li><li>Djame Seddah (Paris-Sorbonne University, France)
+</li><li>Milan Straka (Charles University, Prague, Czech Republic)
+</li><li>Michael White (Ohio State University, Columbus, USA)
+</li><li>Nianwen Xue (Brandeis University, Waltham, USA)
+</li><li>Daniel Zeman (Charles University, Prague, Czech Republic)
+</li><li>Heike Zinsmeister (University of Hamburg, Germany)
+</li>
+</ul>
+
+<h2>Local organizing committee</h2>
+<ul>
+	<li>Jan Hajič (Charles University, Czech Republic)</li>
+	<li>Jiří Balhar (Charles University, Czech Republic)</li>
+	<li>Eduard Bejček (Charles University, Czech Republic)</li>
+	<li>Kateřina Bryanová (Charles University, Czech Republic)</li>
+	<li>Jan Ptáček (Charles University, Czech Republic)</li>
+	<li>Zdeňka Urešová (Charles University, Czech Republic)</li>
+</ul>
 </article>
 
 <?php include("parts/footer.php"); ?>
