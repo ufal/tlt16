9da0055c939fcd7417cbdc373d9ce1db77b425ae
Jan Hajic <hajic@ufal.mff.cuni.cz>
2017-06-23 10:58:27 +0200

    Update important-dates.php

--- parts/important-dates.php
+++ parts/important-dates.php
@@ -1,5 +1,5 @@
 <ul>
-	<li>10 June 2017: Submission system open</li>
+	<li>23 June 2017: Submission system open</li>
 	<li>9 November 2017: Deadline for submissions, registration open </li>
 	<li>21 November 2017: Notification of acceptance</li>
 	<li>11 December 2017: Final papers due </li>
