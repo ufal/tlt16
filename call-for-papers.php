3366d39fbd40292ca1b96db0ff1f5d76497e29fb
Jan Hajic <hajic@ufal.mff.cuni.cz>
2017-06-23 10:57:41 +0200

    Update call-for-papers.php

--- call-for-papers.php
+++ call-for-papers.php
@@ -10,29 +10,35 @@ viewPageHeader($title);
 viewArticleHeader($title);
 ?>
 
-<p>TLT serves as a venue for new and ongoing research on the topic of linguistics and treebanks. The 16th edition of TLT will, for the second time, take place in Prague, Czech Republic, at Charles University in the heart of the old city, on 23-24 January 2018.</p>
+<p>TLT serves as a venue for new and ongoing research on the topic of linguistics and treebanks. The 16th edition of TLT will, for the second time, take place in Prague, Czech Republic, at Charles University in the heart of the old city, on 23-24 January 2018.
+</p>
 
-<p>For over 15 years now, TLT has served as a venue for new and ongoing high-quality work related to syntactically-annotated corpora, i.e., treebanks; with a focus on all the aspects of treebanking – descriptive, theoretical, formal and computational – but also going beyond treebanks, including other levels of annotation such as frame semantics and similar formalisms, coreference, discourse, named entities or events, to name only a few.</p>
+<p>For over 15 years now, TLT has served as a venue for new and ongoing high-quality work related to syntactically-annotated corpora, i.e., treebanks; with a focus on all the aspects of treebanking – descriptive, theoretical, formal and computational – but also going beyond treebanks, including other levels of annotation such as frame semantics and similar formalisms, coreference, discourse, named entities or events, to name only a few. 
+</p>
 
-<p>Submissions are invited for papers, posters and demonstrations which present research on treebanks and their intersection with linguistics, natural language processing and other related fields.</p>
+<p>Submissions are invited for papers, posters and demonstrations which present research on treebanks and their intersection with linguistics, natural language processing and related fields. 
+</p>
 
 <p>TLT16 website: <a href="http://ufal.mff.cuni.cz/tlt16" title="TLT16 website">http://ufal.mff.cuni.cz/tlt16</a></p>
 
-<h2>Invited speakers</h2>
+<h2>Invited speaker</h2>
 
 <ul>
 	<li>TBD</li>
 </ul>
 
-<h2>Workshop motivation and aims</h2>
+<h2>Motivation and aims</h2>
 
-<p>Treebanks have proved to be crucial resources for very important NLP applications, such as machine translation and information extraction, as well as supporting resources for various NLP tasks, such as high-quality parsing and POS tagging.</p>
+<p>Treebanks have proved to be crucial resources for very important NLP applications, such as machine translation and information extraction, as well as supporting resources for various NLP tasks, such as high-quality parsing and POS tagging. 
+</p>
 
-<p>More recent trends in treebank-related research have focused on a variety of initiatives, such as annotating deep syntactic, semantic, and syntactic-semantic interface information; (semi)-automatic conversion of existing treebanks into deeper linguistic formats; multilingual and crosslingual treebanking, including 'language-universal' treebanking; typology and its relation to treebanking; enriching treebanks with additional layers of linguistic annotation as well as incorporating world knowledge; dynamic treebanking involving a close connection between grammar-based parsing and manual annotation; designing web services for observing and exploiting theoretically diverse treebanks; and mapping syntactic and semantic knowledge to Linked Open Data (LOD) information.</p>
+<p>More recent trends in treebank-related research have focused on a variety of initiatives, such as annotating deep syntactic, semantic, and syntactic-semantic interface information; (semi)-automatic conversion of existing treebanks into deeper linguistic formats; multilingual and cross-lingual treebanking, including 'language-universal' treebanking; typology and its relation to treebanking; enriching treebanks with additional layers of linguistic annotation as well as incorporating world knowledge; dynamic treebanking involving a close connection between grammar-based parsing and manual annotation; designing web services for observing and exploiting theoretically diverse treebanks; and mapping syntactic and semantic knowledge to Linked Open Data (LOD) information. 
+</p>
 
-<h2>Workshop topics</h2>
+<h2>Topics</h2>
 
-<p>The workshop invites the submission of papers, posters and software demonstrations on original and unpublished research on the following topics, including, but not limited to:</p>
+<p>TLT16 invites the submission of papers, posters and software demonstrations on original and unpublished research on the following topics, including, but not limited to: 
+</p>
 <ul>
 	<li>Design principles and annotation schemes for treebanks </li>
 	<li>The use of treebanks in acquiring linguistic knowledge </li>
@@ -59,20 +65,33 @@ viewArticleHeader($title);
 
 <p>This year, we specifically invite papers on all aspects of the relation of linguistically motivated complex annotation, such as treebanking, and deep learning methods. Topics of such papers may include, but are not limited to:</p>
 <ul>
-	<li>a comparison of application performance using a linguistic representation vs. end-to-end Deep Neural Network system, or any particular aspects of it (e.g. differences in recall vs. precision)</li>
+	<li>a comparison of application performance using a linguistic representation vs. end-to-end Deep Neural Network system, or any particular aspects of it (e.g., differences in recall vs. precision)</li>
 	<li>analysis of errors made by a deep-learning system vs. a system using linguistic features or representation</li>
 	<li>approaches to "soften" categorical annotation usually present in treebanks by using distributional methods</li>
 	<li>specific aspects of deep learning when applied to complex treebanks</li>
-	<li>advantages or disadvantages of deep learning from treebanks in multilingual setting.  </li>
+	<li>advantages or disadvantages of deep learning from treebanks in a multilingual setting.  </li>
 </ul>
 
 <h2>Instructions for authors</h2>
 
-<p>The program for TLT16 will consist of full papers, posters and software demonstrations. This year, we ask authors not to anonymize their submissions; we believe that full anonymity is a difficult task, since many papers now appear as preprints on the web and it is easy to find authors; for demonstrations, it is almost impossible in any case due to the use of system names, references to previous version etc. Submissions must be written in English, and conform to the TLT16 style files available at <a href="http://ufal.mff.cuni.cz/tlt16/">http://ufal.mff.cuni.cz/tlt16/</a>.</p>
+<p>The program for TLT16 will consist of full papers, posters and software demonstrations. 
+	This year, we ask authors <b>not</b> to anonymize their submissions; 
+we believe that full anonymity is difficult to achieve, since many papers now appear as preprints on the web and it is easy to find authors; 
+	for demonstrations, it is almost impossible in any case due to the use of system names, references to previous versions etc. 
+	Submissions must be written in English, and conform to the TLT16 style files available at
+<a href="http://ufal.mff.cuni.cz/tlt16/style-tlt16.zip">http://ufal.mff.cuni.cz/tlt16/style-tlt16.zip</a>.</p>
 
-<p>All submissions should be full-length papers of up to 7 pages plus unlimited references. It is assumed that system demonstration papers might be shorter; in any case, use an appropriate length given what is the idea you are conveying in your contribution and the amount of technical details and/or examples you want to put in. Accepted final papers will get an extra page (8 pages total) plus unlimited number of pages for references and up to 4 pages for an optional appendix which could serve to publish algorithms, code snippets and/or additional examples.</p>
+<p>All submissions should be full-length papers of up to 7 pages plus unlimited references. 
+It is assumed that system demonstration papers might be shorter; 
+	in any case, use an appropriate length given what is the idea you are conveying in your contribution and the amount of technical details and/or examples you want to put in. 
+	Accepted final papers will get an extra page (8 pages total) plus unlimited number of pages for references 
+	and up to 4 pages for an optional appendix which could serve to publish algorithms, code snippets 
+	and/or additional examples.</p>
 
-<p>The organizers will work with the ACL Anthology to list the TLT proceedings there. Authors of outstanding papers as identified by the PC members will be invited to submit an extended version to The Prague Bulletin of Mathematical Linguistics (<a href="https://ufal.mff.cuni.cz/pbml">https://ufal.mff.cuni.cz/pbml</a>), an ERIH-Plus listed journal published online by deGruyter in cooperation with Charles University.</p>
+<p>The organizers will work with the ACL Anthology to list the TLT proceedings there. 
+	Authors of outstanding papers as identified by the PC members will be invited to submit an extended version 
+	to The Prague Bulletin of Mathematical Linguistics (<a href="https://ufal.mff.cuni.cz/pbml">https://ufal.mff.cuni.cz/pbml</a>), 
+	an ERIH-Plus listed journal published online by deGruyter in cooperation with Charles University.</p>
 
 <p>Please upload your submissions via EasyChair: <a href="https://easychair.org/conferences/?conf=tlt16">https://easychair.org/conferences/?conf=tlt16</a></p>
 
@@ -84,52 +103,54 @@ viewArticleHeader($title);
 	<li>Jan Hajic (Charles University in Prague, Czech Republic)</li>
 	<li>Sandra Kübler (Indiana University, USA)</li>
 	<li>Stephan Oepen (University of Oslo, Norway) </li>
-	<li>Markuc Dickinson (Indiana University, USA)</li>
+	<li>Markus Dickinson (Indiana University, USA)</li>
 </ul>
 
 <h2>Program committee </h2>
 
 <ul>
-	<li>Memduh Gokirmak</li>
-	<li>Silvie Cinková</li>
-	<li>Barbora Hladká</li>
-	<li>Milan Straka</li>
-	<li>Tomaž Erjavec</li>
-	<li>Filip Ginter</li>
-	<li>Adam Meyers</li>
-	<li>Emad Mohamed</li>
-	<li>Olga Scrivner</li>
-	<li>Marie-Catherine de Marneffe</li>
-	<li>Patricia Amaral</li>
-	<li>Ann Bie</li>
-	<li>Miriam Butt</li>
-	<li>Tatjana Scheffler</li>
-	<li>Lilja Øvrelid</li>
-	<li>Dag Haug</li>
-	<li>Emily Bender</li>
-	<li>Joakim Nivre</li>
-	<li>Petya Osenova</li>
-	<li>Agnieszka Patejuk</li>
-	<li>Djamé Seddah</li>
-	<li>Nianwen Xue</li>
-	<li>Daniel Zeman</li>
-	<li>Heike Zinsmeister</li>
-	<li>Eckhard Bick</li>
-	<li>Gosse Bouma</li>
-	<li>Jinho Choi</li>
-	<li>Koenraad De Smedt</li>
-	<li>Eva Hajičová</li>
-	<li>Lori Levin</li>
-	<li>Teresa Lynn</li>
-	<li>Jiří Mírovský</li>
-	<li>Kaili Müürisep</li>
+<li>Patricia Amaral (Indiana University, Bloomington, USA)
+	</li><li>Emily Bender (University of Washington, Seattle, USA)
+</li><li>Eckhard Bick (University of Southern Denmark, Denmark)
+</li><li>Ann Bies (Linguistic Data Consortium, Univ. of Pennsylvania, USA)
+</li><li>Gosse Bouma (University of Groningen, The Netherlands)
+</li><li>Miriam Butt (University of Konstanz, Germany)
+</li><li>Jinho Choi (Emory University, Atlanta, Georgia, USA)
+</li><li>Silvie Cinkova (Charles University, Prague, Czech Republic)
+</li><li>Koenraad De Smedt (Bergen University, Norway)
+</li><li>Tomaz Erjavec (Institut Jozef Stefan, Ljubljana, Slovenia)
+</li><li>Filip Ginter (Turku University, Finland)
+</li><li>Memduh Gokirmak (Instanbul University, Turkey)
+</li><li>Eva Hajicova (Charles University, Prague, Czech Republic)
+</li><li>Dag Haug (University of Oslo, Norway)
+</li><li>Barbora Hladka (Charles University, Prague, Czech Republic)
+</li><li>Lori Levin (Carnegie Mellon University, Pittsburgh, Pennsylvania, USA)
+</li><li>Teresa Lynn (Dublin City University, Ireland)
+</li><li>Marie-Catherine de Marneffe (Ohio State University, Columbus, USA)
+</li><li>Adam Meyers (New York University, New York, USA)
+</li><li>Emad Mohamed (Indiana University, Bloomington, USA)
+</li><li>Jiri Mirovsky (Charles University, Prague, Czech Republic)
+</li><li>Kaili Muurisep (University of Tartu, Estonia)
+</li><li>Joakim Nivre (Uppsala University, Sweden)
+</li><li>Petya Osenova (Bulgarian Academy of Sciences, Sofia, Bulgaria)
+</li><li>Lilja Ovrelid (University of Oslo, Norway)
+</li><li>Agnieszka Patejuk (Polish Academy of Sciences, Warsaw, Poland)
+</li><li>Tatjana Scheffler (University of Potsdam, Germany)
+</li><li>Olga Scrivner (University of Indiana, Bloomington, USA)
+</li><li>Djame Seddah (Paris-Sorbonne University, France)
+</li><li>Milan Straka (Charles University, Prague, Czech Republic)
+</li><li>Michael White (Ohio State University, Columbus, USA)
+</li><li>Nianwen Xue (Brandeis University, Waltham, USA)
+</li><li>Daniel Zeman (Charles University, Prague, Czech Republic)
+</li><li>Heike Zinsmeister (University of Hamburg, Germany)
+</li>
 </ul>
 
 <h2>Local organizing committee</h2>
 <ul>
 	<li>Jan Hajič (Charles University, Czech Republic)</li>
 	<li>Jiří Balhar (Charles University, Czech Republic)</li>
-	<li>Eduard Bejcek (Charles University, Czech Republic)</li>
+	<li>Eduard Bejček (Charles University, Czech Republic)</li>
 	<li>Kateřina Bryanová (Charles University, Czech Republic)</li>
 	<li>Jan Ptáček (Charles University, Czech Republic)</li>
 	<li>Zdeňka Urešová (Charles University, Czech Republic)</li>
