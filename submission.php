7365f10f9e8dabe9a8fc3e042507bbe684503c43
Jan Hajic <hajic@ufal.mff.cuni.cz>
2017-06-23 11:08:33 +0200

    Update submission.php

--- submission.php
+++ submission.php
@@ -10,6 +10,14 @@ viewPageHeader($title);
 viewArticleHeader($title);
 ?>
 
+  
+<p>Please upload your submissions via EasyChair: <a href="https://easychair.org/conferences/?conf=tlt16">https://easychair.org/conferences/?conf=tlt16</a></p>
+
+<h2>Important dates</h2>
+<?php include("parts/important-dates.php"); ?>
+         
+For details, please see <a href="http://ufal.mff.cuni.cz/tlt16/call-for-papers.php">http://ufal.mff.cuni.cz/tlt16/call-for-papers.php</a>.
+  
 </article>
 
 <?php include("parts/footer.php"); ?>
