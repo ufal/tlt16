c3726aca6a4a181be0b5e2d5b863d6ca1fe1fd25
Jan Hajic <hajic@ufal.mff.cuni.cz>
2017-11-03 13:43:47 +0100

    Update important-dates.php

--- parts/important-dates.php
+++ parts/important-dates.php
@@ -1,7 +1,7 @@
 <ul>
 	<li>23 June 2017: Submission system open</li>
-	<li>9 November 2017: Deadline for submissions, registration open </li>
-	<li>21 November 2017: Notification of acceptance</li>
-	<li>11 December 2017: Final papers due </li>
+	<li>14 <del>9</del> November 2017: Deadline for submissions, registration open </li>
+	<li>26 <del>21</del> November 2017: Notification of acceptance</li>
+	<li>12 <del>11</del> December 2017: Final papers due </li>
 	<li>23-24 January 2018: The conference</li>
 </ul>
